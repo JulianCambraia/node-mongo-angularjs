3b309aa19a1ccfb0b4f7c47c7e0c6b5a1e40add4
JulianCambraia <juliancambraia@gmail.com>
2020-07-26 18:31:21 -0300

    proteje as requisições na API

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -2,6 +2,6 @@ module.exports = function(req, res, next) {
     // habilitar no cabeçalho
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 }
