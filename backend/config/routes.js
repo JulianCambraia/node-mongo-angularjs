0dc234f11da40a09c0469e0aaf351c093bdd801b
JulianCambraia <juliancambraia@gmail.com>
2020-07-26 18:53:21 -0300

    proteger as requisições com JWT

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const auth = require('./auth');
 
 module.exports = function(server) {
-    // API Routers
-    const router = express.Router();
-    server.use('/api', router);
+    // API Rotas Abertas
+    const openApi = express.Router();
+    server.use('/oapi', openApi);
+
+    const AuthService = require('../api/user/authService');
+    
+    openApi.post('/login', AuthService.login);
+    openApi.post('/signup', AuthService.signup);
+    openApi.post('/validateToken', AuthService.validateToken);
+
+    /**
+     * Rotas protejidas por Token JWT
+     * Ou seja qualquer requisição que passar pela Api da url será protegida.
+     */
+
+     const protectedApi = express.Router();
+     server.use('/api', protectedApi);
+
+     protectedApi.use(auth);
 
     const billingCycleService = require('../api/billingCycle/billingCycleService');
     // todas as requisições serão registradas na url raiz -> billingCycles
-    billingCycleService.register(router, '/billingCycles');
+    billingCycleService.register(protectedApi, '/billingCycles');
 
     const billingSummaryService = require('../api/billingSummary/billingSummaryService');
-    router.route('/billingSummary').get(billingSummaryService.getSummary);
+    protectedApi.route('/billingSummary').get(billingSummaryService.getSummary);
 
 }
